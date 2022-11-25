b34f35ec412726dd06f7b5ea4a296c8b17cc3e74
ibrahimatay <contact@ibrahimatay.com>
2022-11-25 23:48:43 +0300

    JEP 323: Local-Variable Syntax for Lambda Parameters

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Examples of the Java programming language feature.
   * [JEP 181](https://openjdk.java.net/jeps/181): Nest-Based Access Control
   * [JEP 318](https://openjdk.java.net/jeps/318): Epsilon: A No-Op Garbage Collector
   * [JEP 321](https://openjdk.java.net/jeps/321): HTTP Client (Standard)
+  * [JEP 323](https://openjdk.java.net/jeps/323): Local-Variable Syntax for Lambda Parameters
   * [JEP 327](https://openjdk.java.net/jeps/327): Unicode 10
   * [JEP 329](https://openjdk.java.net/jeps/329): ChaCha20 and Poly1305 Cryptographic Algorithms
   * [JEP 332](https://openjdk.java.net/jeps/332): Transport Layer Security (TLS) 1.3
--- java-11/src/main/java/com/ibrahimatay/JEP323LocalVariableSyntaxForLambdaParameters.java
+++ java-11/src/main/java/com/ibrahimatay/JEP323LocalVariableSyntaxForLambdaParameters.java
@@ -4,7 +4,7 @@ import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class JEP323LocalVar {
+public class JEP323LocalVariableSyntaxForLambdaParameters {
     public static void main(String[] args) {
         // JEP 323: Local-Variable Syntax for Lambda Parameters
         // https://openjdk.org/jeps/323
@@ -17,6 +17,9 @@ public class JEP323LocalVar {
 
         // Update on JEP-286 (Local Variable Type Inference)
         // https://mail.openjdk.org/pipermail/platform-jep-discuss/2016-December/000066.html
+
+        // Java 11: Local-Variable Syntax for Lambda Parameters - applications
+        // https://stackoverflow.com/questions/50225672/java-11-local-variable-syntax-for-lambda-parameters-applications
 
         var alphabet = Arrays.asList("A", "B", "C", "D", "E");
         var result01 = alphabet.stream()
