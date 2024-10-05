1658c17f2a21dc6c653c6821f2f080cde2440261
ibrahimatay <contact@ibrahimatay.com>
2024-10-05 19:39:46 +0000

    JEP 467: Markdown Documentation Comments

---
+++ java-23/src/main/java/com/ibrahimatay/JEP467MarkdownDocumentationComments.java
@@ -0,0 +1,26 @@
+package com.ibrahimatay;
+
+/**
+ * # JEP467MarkdownDocumentationComments Class
+ *
+ * This class provides basic arithmetic operations.
+ *
+ * ## Methods
+ *
+ * - `add(int a, int b)`: Returns the sum of two integers
+ * - `subtract(int a, int b)`: Returns the difference between two integers
+ *
+ * ## Example Usage
+ *
+ * java
+ * Calculator calc = new Calculator();
+ * int sum = calc.add(5, 3);  // Returns 8
+ *
+ *
+ * @author İbrahim ATAY
+ * @version 1.0
+ */
+public class JEP467MarkdownDocumentationComments {
+    // JEP 467: Markdown Documentation Comments
+    // https://openjdk.org/jeps/467
+}
