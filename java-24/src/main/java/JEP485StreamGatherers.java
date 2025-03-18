aac716f73d9b49198fc74744c3cf884fdf1987ce
ibrahimatay <contact@ibrahimatay.com>
2025-03-18 21:51:14 +0300

    version problem fixed

--- java-24/src/main/java/JEP485StreamGatherers.java
+++ java-24/src/main/java/JEP485StreamGatherers.java
@@ -8,21 +8,27 @@ public class JEP485StreamGatherers {
         // https://openjdk.java.net/jeps/485
 
         // Custom Intermediate Operation distinctBy
+        /*
         Stream.of("foo", "bar", "baz", "quux")
                 .gather(Gatherers.distinctBy(String::length))
                 .toList();
+        */
 
         // Creating Fixed-size Windows
+        /*
         Stream.iterate(0, i -> i + 1)
                 .gather(Gatherers.windowFixed(3))
                 .limit(2)
                 .toList();
+         */
 
         // Parallel Processing with selectOne
+        /*
         Stream.generate(() -> ThreadLocalRandom.current().nextInt())
                 .limit(1000)
                 .parallel()
                 .gather(Gatherers.selectOne(Math::max))
                 .findFirst();
+         */
     }
 }
