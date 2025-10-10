59ecf2370f24960c821ddd74d0589a5408f92442
ibrahimatay <contact@ibrahimatay.com>
2025-10-10 22:26:13 +0300

    JEP 513: Flexible Constructor Bodies

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ This repository contains Java examples that are designed to track and document t
 ## Specifications & Practices
 
 * [Java 25](java-25/) (September, 2025)
-  * [JEP 512](java-25/src/main/java/JEP512CompactSourceFilesAndInstanceMainMethods.java): Compact Source Files and Instance Main Methods
+    * [JEP 513](java-25/src/main/java/JEP513FlexibleConstructorBodies.java): Flexible Constructor Bodies
+    * [JEP 512](java-25/src/main/java/JEP512CompactSourceFilesAndInstanceMainMethods.java): Compact Source Files and Instance Main Methods
   * [JEP 511](java-25/src/main/java/JEP511ModuleImportDeclarations.java): Module Import Declarations
+
 * [Java 24](java-24/) (March, 2025)
   * [JEP 488](java-24/src/main/java/JEP488PrimitiveTypesInPatternsInstanceofAndSwitch.java): Primitive Types in Patterns, instanceof, and switch
   * [JEP 495](java-24/src/main/java/JEP495SimpleSourceFilesAndInstanceMainMethods.java): Simple Source Files and Instance Main Methods
---
+++ java-25/src/main/java/JEP513FlexibleConstructorBodies.java
@@ -0,0 +1,46 @@
+// JEP 513: Flexible Constructor Bodies
+// https://openjdk.org/jeps/513
+
+// Support JEP 513: Flexible Constructor Bodies
+// https://youtrack.jetbrains.com/projects/IDEA/issues/IDEA-372971/Support-JEP-513-Flexible-Constructor-Bodies
+
+void main(String[] args) {
+    Employee e1 = new Employee(30, "A123");
+    e1.show();
+
+    // Employee Age: 30, Office ID: null
+    // Employee Age: 30, Office ID: A123
+}
+
+class Person {
+    final int age;
+
+    Person(int age) {
+        this.age = age;
+        show();
+    }
+
+    void show() {
+        System.out.println("Age: " + age);
+    }
+}
+
+class Employee extends Person {
+    final String officeId;
+
+    Employee(int age, String officeId) {
+        super(validateAge(age));
+        this.officeId = officeId;
+    }
+
+    private static int validateAge(int age) {
+        if (age < 18 || age > 67)
+            throw new IllegalArgumentException("Invalid age: " + age);
+        return age;
+    }
+
+    @Override
+    void show() {
+        System.out.println("Employee Age: " + age + ", Office ID: " + officeId);
+    }
+}
