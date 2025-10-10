336bd9da945a430c3b53e9414504b01ea5ff52f2
ibrahimatay <contact@ibrahimatay.com>
2025-10-10 21:49:41 +0300

    java-25

---
+++ java-25/src/main/java/com/ibrahimatay/Main.java
@@ -0,0 +1,16 @@
+package com.ibrahimatay;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(String.format("Hello and welcome!"));
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
